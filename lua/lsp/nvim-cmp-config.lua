efcdfbba33e8fb5b3cbafbd8f97e73573cf45db8
lee linfeng <2033211717@qq.com>
2022-01-25 15:41:33 +0800

    delete:vsnip

--- lua/lsp/nvim-cmp-config.lua
+++ lua/lsp/nvim-cmp-config.lua
@@ -50,8 +50,8 @@ local has_words_before = function()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif vim.fn["vsnip#available"](1) == 1 then
-          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        --elseif vim.fn["vsnip#available"](1) == 1 then
+          --feedkey("<Plug>(vsnip-expand-or-jump)", "")
         elseif has_words_before() then
           cmp.complete()
         else
